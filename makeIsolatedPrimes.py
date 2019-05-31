bdc94be679b3f3ad4d6e234543c63b80899be8b1
Rick Gutleber <rickg@his.com>
2019-05-31 12:22:34 -0400

    Update makeIsolatedPrimes.py

--- makeIsolatedPrimes.py
+++ makeIsolatedPrimes.py
@@ -14,34 +14,26 @@ from primeDataUtils import outputDirectory, readPrimeNumbers, updateOutputInterv
 def main( ):
     lineCount = 1
 
-    primesSize = 5
+    primesSize = 3
 
-    primes = [ ]
-    diffs = [ ]
-    adiffs = [ ]
-
-    for i in range( 0, primesSize ):
-        primes.append( [ -9999999, -9999999 ] )
-
-    for i in range( 0, primesSize - 1 ):
-        diffs.append( 0 )
-        adiffs.append( 0 )
+    primes = [ [ -9999999, -9999999 ] ] * primesSize
+    diffs = [ 0 ] * ( primesSize - 1 )
 
     isolatedIndex = 0
 
     numberOfTypes = 100
 
     isolatedIndex = [ 0 ] * numberOfTypes
-    isolatedFile = [ ]
+    outputInterval = [ 1 ] * numberOfTypes
+
+    isolatedFiles = [ ]
 
     for i in range( 0, numberOfTypes ):
-        isolatedFile.append( open( outputDirectory + os.sep + 'isolated{:03}_primes.txt'.format( i * 2 + 4 ), 'w' ) )
+        isolatedFiles.append( open( outputDirectory + os.sep + 'isolated{:03}_primes.txt'.format( i * 2 + 4 ), 'w' ) )
 
     printInterval = 100000
 
-    outputInterval = [ 1 ] * numberOfTypes
-
-    for index, prime in readPrimeNumbers( 1000000000 ):
+    for index, prime in readPrimeNumbers( 4000000000 ):
         primes.append( [ index, prime ] )
         del primes[ 0 ]
 
@@ -52,7 +44,6 @@ def main( ):
 
         for i in range( 0, primesSize - 1 ):
             sum += diffs[ i ]
-            adiffs[ i ] = sum
 
         if index % printInterval == 0:
             print( '\r{:,}'.format( index ), end='' )
@@ -60,13 +51,15 @@ def main( ):
         for i in range( 0, numberOfTypes ):
             diff = i * 2 + 4
 
-            if diffs[ 0 ] > diff and diffs[ 1 ] > diff:
+            if ( diffs[ 0 ] >= diff ) and ( diffs[ 1 ] >= diff ):
                 isolatedIndex[ i ] += 1
-                outputInterval[ i ] = updateOutputInterval( isolatedIndex[ i ], outputInterval[ i ] )
-                isolatedFile[ i ].write( '{},{}\n'.format( isolatedIndex[ i ], primes[ 1 ][ 1 ] ) )
+
+                if ( isolatedIndex[ i ] % outputInterval[ i ] == 0 ):
+                    outputInterval[ i ] = updateOutputInterval( isolatedIndex[ i ], outputInterval[ i ] )
+                    isolatedFiles[ i ].write( '{:12} {}\n'.format( isolatedIndex[ i ], primes[ 1 ][ 1 ] ) )
 
     for i in range( 0, numberOfTypes ):
-        isolatedFile[ i ].close( )
+        isolatedFiles[ i ].close( )
 
 
 #//******************************************************************************
