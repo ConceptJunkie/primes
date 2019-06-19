8101acea0bd864cbbb650884a8b6221d9bf4426c
Rick Gutleber <rickg@his.com>
2019-06-19 11:58:06 -0400

    check up instead of down
    It's slower, but we'll get all 60 billion primes this way.

--- makeSophiePrimes.py
+++ makeSophiePrimes.py
@@ -24,13 +24,14 @@ def main( ):
     sophieFile = open( outputDirectory + os.sep + 'sophie_primes.txt', 'w' )
 
     for index, prime in readPrimeNumbers( 60000000000 ):
-        sophie = int( ( prime - 1 ) / 2 )
+        #sophie = int( ( prime - 1 ) / 2 )
+        safe = ( prime * 2 ) + 1
 
-        if isPrime( sophie ):
+        if isPrime( safe ):
             sophieIndex += 1
 
             if sophieIndex % outputInterval == 0:
-                sophieFile.write( '{:12} {}\n'.format( sophieIndex, sophie ) )
+                sophieFile.write( '{:12} {}\n'.format( sophieIndex, prime ) )
                 outputInterval = updateOutputInterval( sophieIndex, outputInterval )
 
         if index % printInterval == 0:
